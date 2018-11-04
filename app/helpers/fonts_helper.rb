53d431caa8b977cc75b7c714fcd720ea9f4ff587
Matt Powell <fauxparse@gmail.com>
2018-11-05 08:20:30 +1300

    fix fonts helper

--- app/helpers/fonts_helper.rb
+++ app/helpers/fonts_helper.rb
@@ -1,22 +1,21 @@
 module FontsHelper
   FONTS = %w(Brother Blokk).freeze
-  WORK_SANS = "@import url('https://fonts.googleapis.com/css?family=Work+Sans:300,400,600');"
+  WORK_SANS = 'https://fonts.googleapis.com/css?family=Work+Sans:300,400,600'.freeze
 
   def include_fonts
-    content_tag :style, type: 'text/css' do
-      FONTS.each do |font|
-        concat <<~STYLES.html_safe
-          @font-face {
-            font-family: '#{font}';
-            src: url(#{font_url("#{font.underscore}.woff2")}) format('woff2'),
-                  url(#{font_url("#{font.underscore}.woff")}) format('woff');
-            font-weight: 700;
-            font-style: normal;
-          }
-        STYLES
+    stylesheet_link_tag(WORK_SANS) +
+      content_tag(:style, type: 'text/css') do
+        FONTS.each do |font|
+          concat <<~STYLES.html_safe
+            @font-face {
+              font-family: '#{font}';
+              src: url(#{font_url("#{font.underscore}.woff2")}) format('woff2'),
+                    url(#{font_url("#{font.underscore}.woff")}) format('woff');
+              font-weight: 700;
+              font-style: normal;
+            }
+          STYLES
+        end
       end
-
-      concat WORK_SANS.html_safe
-    end
   end
 end
