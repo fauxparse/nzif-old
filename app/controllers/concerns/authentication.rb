2174958c49fe5eb91205c30fca22744e13813632
Matt Powell <fauxparse@gmail.com>
2018-11-24 22:45:53 +1300

    switch to JWT and cookies for authentication
    ActionCable doesn't have access to the controller session (of course),
    but it does like cookies.

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,7 +1,29 @@
+require 'jwt'
+
 module Authentication
   extend ActiveSupport::Concern
 
-  SESSION_KEY = :current_user_id
+  COOKIE = :current_user_id
+
+  class TokenEncoder
+    def encode(id)
+      return nil if id.blank?
+      JWT.encode({ id: id }, secret)
+    end
+
+    def decode(token)
+      return nil if token.blank?
+      JWT.decode(token, secret)[0]['id']
+    rescue
+      nil
+    end
+
+    private
+
+    def secret
+      Rails.application.credentials.secret_key_base
+    end
+  end
 
   included do
     helper_method :current_user
@@ -9,16 +31,17 @@ module Authentication
   end
 
   def current_user
-    @current_user ||= current_user_id && User.find_by(id: current_user_id)
+    @current_user ||= User.find_by(id: current_user_id)
   end
 
   def current_user=(user)
-    session[SESSION_KEY] = user&.id
+    @current_user = user
+    cookies[COOKIE] = TokenEncoder.new.encode(user&.id)
   end
 
   private
 
   def current_user_id
-    session[SESSION_KEY]
+    TokenEncoder.new.decode(cookies[COOKIE])
   end
 end
--- spec/controllers/concerns/authentication_spec.rb
+++ spec/controllers/concerns/authentication_spec.rb
@@ -6,48 +6,42 @@ RSpec.describe Authentication do
   end
 
   let(:controller) { AuthenticationTest.new }
-  let(:session) { double(:session) }
+  let(:cookies) { double(:cookies) }
+  let(:cookie) { '[encrypted]' }
+  let(:token_encoder) { instance_double('Authentication::TokenEncoder') }
 
   before do
-    allow(controller).to receive(:session).and_return(session)
+    allow(controller).to receive(:cookies).and_return(cookies)
+    allow(Authentication::TokenEncoder).to receive(:new).and_return(token_encoder)
   end
 
   describe '#current_user' do
-    subject { controller.current_user }
+    subject(:current_user) { controller.current_user }
 
     context 'when a user is logged in' do
       let(:user) { create(:user) }
 
-      before do
-        allow(session).
-          to receive(:[]).
-          with(Authentication::SESSION_KEY).
-          and_return user.id
+      it 'authenticates as the user' do
+        expect(cookies).to receive(:[]).with(Authentication::COOKIE) { cookie }
+        expect(token_encoder).to receive(:decode).with(cookie) { user.id }
+        expect(current_user).to eq user
       end
-
-      it { is_expected.to eq user }
     end
 
     context 'when nobody is logged in' do
-      before do
-        allow(session).
-          to receive(:[]).
-          with(Authentication::SESSION_KEY).
-          and_return nil
+      it 'does not authenticate' do
+        expect(cookies).to receive(:[]).with(Authentication::COOKIE).and_return(nil)
+        expect(token_encoder).to receive(:decode).with(nil).and_return(nil)
+        expect(current_user).to be_nil
       end
-
-      it { is_expected.to be_nil }
     end
 
-    context 'with a stale session' do
-      before do
-        allow(session).
-          to receive(:[]).
-          with(Authentication::SESSION_KEY).
-          and_return 1
+    context 'with a stale cookie' do
+      it 'is nil' do
+        expect(cookies).to receive(:[]).with(Authentication::COOKIE) { cookie }
+        expect(token_encoder).to receive(:decode).with(cookie).and_return(1)
+        expect(current_user).to be_nil
       end
-
-      it { is_expected.to be_nil }
     end
   end
 
@@ -55,22 +49,18 @@ RSpec.describe Authentication do
     context 'with a user' do
       let(:user) { create(:user) }
 
-      it 'sets the session variable' do
-        expect(session).
-          to receive(:[]=).
-          with(Authentication::SESSION_KEY, user.id).
-          and_return nil
+      it 'sets the cookie variable' do
+        expect(token_encoder).to receive(:encode).with(user.id) { cookie }
+        expect(cookies).to receive(:[]=).with(Authentication::COOKIE, cookie)
 
         controller.current_user = user
       end
     end
 
     context 'with no user' do
-      it 'sets the session variable' do
-        expect(session).
-          to receive(:[]=).
-          with(Authentication::SESSION_KEY, nil).
-          and_return nil
+      it 'sets the cookie variable' do
+        expect(token_encoder).to receive(:encode).with(nil).and_return(nil)
+        expect(cookies).to receive(:[]=).with(Authentication::COOKIE, nil)
 
         controller.current_user = nil
       end
