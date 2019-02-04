5f6e05cf4942247104868d001933e73c8e07cc30
Matt Powell <fauxparse@gmail.com>
2019-02-04 18:01:59 +1300

    remove the last vestiges of styled components

--- app/javascript/themes/gradients.js
+++
@@ -1,8 +0,0 @@
-import colors from './colors'
-
-const gradient = (from, to) =>
-  `linear-gradient(to right bottom, ${from}, ${to})`
-
-export default {
-  primary: gradient(colors.palette.tomato(500), colors.palette.mandarin(600)),
-}
--- app/javascript/themes/layout.js
+++
@@ -1,6 +0,0 @@
-export default {
-  borderRadius: '0.25em',
-  padding: '1rem',
-  medium: '40rem',
-  width: '60rem',
-}
