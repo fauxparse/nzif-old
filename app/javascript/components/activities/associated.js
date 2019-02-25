f8c1ee44fed20f68702c0daa0a8304fb186e75bf
Matt Powell <fauxparse@gmail.com>
2019-02-26 11:06:59 +1300

    hide related activities with no sessions

--- app/javascript/components/activities/associated.js
+++ app/javascript/components/activities/associated.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import CommonProps from '../../lib/common_props'
 import Link from '../shared/text_link'
 import Date from '../shared/date'
 
-const AssociatedShow = ({ name, url, session }) => <>
-  Participants in this workshop may be considered for casting in the show{' '}
-  <Link to={url}>{name}</Link>,{' '}
-  on <Date date={session.startsAt} />.
-</>
+const AssociatedShow = ({ name, url, session }) => (
+  <>
+    Participants in this workshop may be considered for casting in the show{' '}
+    <Link to={url}>{name}</Link>, on <Date date={session.startsAt} />.
+  </>
+)
 
-const AssociatedWorkshop = ({ name, url, session }) => <>
-  This show will be cast from the associated workshop,{' '}
-  <Link to={url}>{name}</Link>,{' '}
-  on <Date date={session.startsAt} />.
-</>
+const AssociatedWorkshop = ({ name, url, session }) => (
+  <>
+    This show will be cast from the associated workshop,{' '}
+    <Link to={url}>{name}</Link>, on <Date date={session.startsAt} />.
+  </>
+)
 
 const Associated = ({ type, name, url, sessions }) => {
   const session = sessions[0]
   const Content = type === 'show' ? AssociatedShow : AssociatedWorkshop
-  return (
+  return session ? (
     <div className={`associated associated--${type}`}>
       <Content {...{ name, url, session }} />
     </div>
+  ) : (
+    <Fragment />
   )
 }
 
@@ -30,7 +34,7 @@ Associated.propTypes = {
   type: CommonProps.activityType.isRequired,
   name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-  sessions: PropTypes.arrayOf(PropTypes.any).isRequired,
+  sessions: PropTypes.array.isRequired
 }
 
 export default Associated
