ff1ba8b3f8b289b2713c8e300092ad424db56f29
Matt Powell <fauxparse@gmail.com>
2019-03-08 15:16:54 +1300

    add focus state to checkboxes

--- app/javascript/components/form/checkbox.js
+++ app/javascript/components/form/checkbox.js
@@ -4,11 +4,14 @@ import classNames from 'classnames'
 import Icon from '../icons'
 
 const Checkbox = ({ checked, className, children, ...props }) => (
-  <label className={classNames('checkbox', className, { 'checkbox--checked': checked })}>
+  <label
+    className={classNames('checkbox', className, { 'checkbox--checked': checked })}
+  >
     <input type="checkbox" checked={checked} {...props} />
-    <Icon className="checkbox__icon" viewBox="0 0 24 24">
-      <path className="circle" d="M12 2a10 10 0 0 1 0 20a10 10 0 0 1 0 -20" />
-      <path className="check" d="M17 9l-6 6l-3-3" />
+    <Icon className="checkbox__icon" viewBox="-16 -16 32 32">
+      <circle className="checkbox__focus" cx={0} cy={0} r={13} />
+      <path className="checkbox__circle" d="M0 -10a10 10 0 0 1 0 20a10 10 0 0 1 0 -20" />
+      <path className="checkbox__check" d="M5 -3l-6 6l-3-3" />
     </Icon>
     <div className="checkbox__label">{children}</div>
   </label>
