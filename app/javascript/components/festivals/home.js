459a04f2c37425497a8aafb9e1a9a27f8476243b
Matt Powell <fauxparse@gmail.com>
2019-03-16 16:50:20 +1300

    nicer homepage

--- app/javascript/components/festivals/home.js
+++ app/javascript/components/festivals/home.js
@@ -6,6 +6,8 @@ import PageContent from '../../components/page_content'
 import Background from '../../components/shared/background'
 import Date from '../../components/shared/date'
 import Loader from '../../components/shared/loader'
+import Logo from '../../components/shared/logo'
+import TextLink from '../../components/shared/text_link'
 
 export const HOMEPAGE_FRAGMENT = gql`
   fragment HomepageFragment on Festival {
@@ -28,20 +30,29 @@ const Home = ({ match }) => {
   const { year } = match.params
 
   return (
-    <PageContent className="page-content--no-padding">
+    <PageContent className="homepage page-content--no-padding">
       <Query query={HOMEPAGE_QUERY} variables={{ year }}>
         {({ loading, data: { festival } }) =>
           loading ? (
             <Loader />
           ) : (
-            <Fragment>
-              <Background className="hero-section">
-                <h1>NZIF {festival.year}</h1>
-                <h2>
-                  <Date date={[festival.startDate, festival.endDate]} />
-                </h2>
-              </Background>
-            </Fragment>
+            <>
+              <Logo />
+              <p className="homepage__purpose">
+                <b>Please note: </b>
+                This is the website for Festival participants and practitioners.
+                For public ticket sales, general information, and media enquiries, please visit{' '}
+                <a
+                  className="text-link"
+                  href="http://nzimprovfestival.co.nz/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  our public site
+                </a>
+                .
+              </p>
+            </>
           )
         }
       </Query>
