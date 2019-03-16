fdf3add7c571e77a0e47ae8843442fd3eef34f50
Matt Powell <fauxparse@gmail.com>
2019-03-16 15:59:18 +1300

    tidy footer styles

--- app/javascript/components/footer/index.js
+++ app/javascript/components/footer/index.js
@@ -4,9 +4,12 @@ import Link from './link'
 
 const Footer = ({ match, children }) => (
   <footer className="footer">
-    <Link to={`${match.url}/workshops`} icon="workshop">Workshops</Link>
-    <Link to={`${match.url}/shows`} icon="show">Shows</Link>
-    <Link to={`${match.url}/map`} icon="venue">Map</Link>
+    <div className="footer__links">
+      <Link to={`${match.url}/workshops`} icon="workshop">Workshops</Link>
+      <Link to={`${match.url}/shows`} icon="show">Shows</Link>
+      <Link to={`${match.url}/map`} icon="venue">Map</Link>
+    </div>
+
     {children}
   </footer>
 )
