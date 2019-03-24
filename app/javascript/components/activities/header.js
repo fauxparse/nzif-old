3477b5f50ec6583172c3c3abddd4699211d9dd5d
Matt Powell <fauxparse@gmail.com>
2019-03-24 16:32:32 +1300

    combine presenter origins

--- app/javascript/components/activities/header.js
+++ app/javascript/components/activities/header.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import PropTypes from 'prop-types'
 import pluralize from 'pluralize'
 import stickybits from 'stickybits'
@@ -11,72 +11,81 @@ import Presenter from '../shared/presenter'
 import Sentence from '../shared/sentence'
 import EditButton from './edit_button'
 
-class ActivityHeader extends Component {
-  breadcrumbs = createRef()
+const ActivityHeader = ({ loading, activity }) => {
+  const breadcrumbs = useRef()
+  const sticky = useRef()
 
-  componentDidMount() {
-    stickybits(this.breadcrumbs.current, {
+  useEffect(() => {
+    sticky.current && sticky.current.cleanup()
+    sticky.current = stickybits(breadcrumbs.current, {
       useFixed: true,
-      useGetBoundingClientRect: true
+      useGetBoundingClientRect: true,
     })
-  }
+    return () => sticky.current.cleanup()
+  }, [breadcrumbs])
 
-  render() {
-    const { loading, activity } = this.props
-    return (
-      <header className="activity-header" data-theme="dark">
-        {activity.image && (
-          <Duotone>
-            <Picture
-              className="activity-header__background"
-              sources={[
-                {
-                  srcSet: `${activity.image.thumbnail}, ${activity.image.small} 2x`,
-                  media: '(max-width: 384px)'
-                },
-                {
-                  srcSet: `${activity.image.medium}, ${activity.image.full} 2x`,
-                  media: '(max-width: 960px)'
-                },
-                {
-                  srcSet: activity.image.full
-                }
-              ]}
-              alt={activity.name}
-            />
-          </Duotone>
+  const presenters = useMemo(() => {
+    const { presenters = [] } = activity || {}
+    const differentOrigins = new Set(presenters.map(p => p.origin)).size > 1
+    return presenters.map((presenter, i) => ({
+      ...presenter,
+      origin: differentOrigins || (i === presenters.length - 1) ? presenter.origin : undefined,
+    }))
+  }, [activity])
+
+  return (
+    <header className="activity-header" data-theme="dark">
+      {activity.image && (
+        <Duotone>
+          <Picture
+            className="activity-header__background"
+            sources={[
+              {
+                srcSet: `${activity.image.thumbnail}, ${activity.image.small} 2x`,
+                media: '(max-width: 384px)'
+              },
+              {
+                srcSet: `${activity.image.medium}, ${activity.image.full} 2x`,
+                media: '(max-width: 960px)'
+              },
+              {
+                srcSet: activity.image.full
+              }
+            ]}
+            alt={activity.name}
+          />
+        </Duotone>
+      )}
+      <Breadcrumbs
+        ref={breadcrumbs}
+        className="activity-header__breadcrumbs"
+        back={`/${activity.festival.year}/${activity.type}s`}
+      >
+        <Breadcrumbs.Link to={`/${activity.festival.year}/${activity.type}s`}>
+          {pluralize(activity.type)}
+        </Breadcrumbs.Link>
+      </Breadcrumbs>
+      <EditButton activity={activity} />
+      <Skeleton loading={loading} as="h1" className="activity-header__name">
+        {activity.name}
+      </Skeleton>
+      <Skeleton
+        loading={loading}
+        as="h2"
+        className="activity-header__presenters"
+      >
+        {loading ? (
+          'Presenter names go here'
+        ) : (
+          <Sentence>
+            {presenters.map(presenter => (
+              <Presenter key={presenter.id} {...presenter} />
+            ))}
+          </Sentence>
         )}
-        <Breadcrumbs
-          ref={this.breadcrumbs}
-          className="activity-header__breadcrumbs"
-          back={`/${activity.festival.year}/${activity.type}s`}
-        >
-          <Breadcrumbs.Link to={`/${activity.festival.year}/${activity.type}s`}>
-            {pluralize(activity.type)}
-          </Breadcrumbs.Link>
-        </Breadcrumbs>
-        <EditButton activity={activity} />
-        <Skeleton loading={loading} as="h1" className="activity-header__name">
-          {activity.name}
-        </Skeleton>
-        <Skeleton
-          loading={loading}
-          as="h2"
-          className="activity-header__presenters"
-        >
-          {loading ? (
-            'Presenter names go here'
-          ) : (
-            <Sentence>
-              {activity.presenters.map(presenter => (
-                <Presenter key={presenter.id} {...presenter} />
-              ))}
-            </Sentence>
-          )}
-        </Skeleton>
-      </header>
-    )
-  }
+      </Skeleton>
+    </header>
+  )
 }
 
 ActivityHeader.propTypes = {
