9029715d5f4456c831764920287a39e793a3ee3d
Matt Powell <fauxparse@gmail.com>
2019-04-25 16:38:47 +1200

    make sticky headings respect page resize

--- app/javascript/lib/hooks.js
+++ app/javascript/lib/hooks.js
@@ -1,15 +1,23 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import stickybits from 'stickybits'
 
 export const useSticky = (options = {}, dependencies = []) => {
   const sticky = useRef()
   const stickySection = useRef()
+  const [width, setWidth] = useState()
+
+  const onResize = () => setWidth(stickySection.current.offsetWidth)
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize)
+    return () => window.removeEventListener('resize', onResize)
+  }, [onResize])
 
   useEffect(() => {
     sticky.current && sticky.current.cleanup()
     sticky.current = stickybits(stickySection.current, options)
     return () => sticky.current.cleanup()
-  }, [options, ...dependencies]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [options, width, ...dependencies]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return stickySection
 }
