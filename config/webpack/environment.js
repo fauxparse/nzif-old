038d5aeba1eb8e15b021b34ea003b8c5959379d9
Matt Powell <fauxparse@gmail.com>
2019-05-22 10:57:46 +1200

    exclude node_modules from Babel

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,9 @@ const { environment } = require('@rails/webpacker')
 const erb =  require('./loaders/erb')
 environment.loaders.append('erb', erb)
 
+environment.loaders.delete('nodeModules')
+
 const WebpackAssetsManifest = require('webpack-assets-manifest')
-// environment.splitChunks()
+environment.splitChunks()
 
 module.exports = environment
