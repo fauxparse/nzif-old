31e9bbc0dac3bb96f92c0264f495024355c734ad
Matt Powell <fauxparse@gmail.com>
2019-06-26 08:15:57 +1200

    upgrade storybook to 5.1.9

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,13 +3,13 @@ const { resolve } = require('path')
 const config = require('@rails/webpacker/package/config')
 const environment = require('../config/webpack/environment')
 
-module.exports = (storybookBaseConfig, configType) => {
+module.exports = async ({ config: storybookBaseConfig }) => {
   storybookBaseConfig.resolve.modules.unshift(resolve(config.source_path))
 
   storybookBaseConfig.module.rules.push({
     test: /\.scss$/,
-    loaders: ["style-loader", "css-loader", "sass-loader"],
-    include: resolve(__dirname, "../")
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: resolve(__dirname, '../')
   })
 
   return storybookBaseConfig
