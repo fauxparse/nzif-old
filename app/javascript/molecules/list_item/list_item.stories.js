9920810cdd1de38a23296bd1f6dc7431c821a406
Matt Powell <fauxparse@gmail.com>
2019-06-27 15:30:24 +1200

    divider component

--- app/javascript/molecules/list_item/list_item.stories.js
+++ app/javascript/molecules/list_item/list_item.stories.js
@@ -2,28 +2,31 @@
 
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import Divider from 'atoms/divider'
 import ListItem from './index'
-
-import small from 'atoms/avatar/avatar_small.jpg'
-import medium from 'atoms/avatar/avatar_medium.jpg'
 
 storiesOf('Molecules|ListItem', module)
   .add('Basic', () => (
     <ul className="list" style={{ maxWidth: '24rem' }}>
       <ListItem icon="user" primary="Profile" />
       <ListItem icon="admin" primary="Settings" secondaryIcon="chevron-right" />
+      <Divider />
       <ListItem primary="Text only" />
     </ul>
   ))
   .add('Avatar', () => (
     <ul className="list" style={{ maxWidth: '24rem' }}>
       <ListItem
+        primary="Jennifer O’Sullivan"
+        secondary="Festival director"
+        avatar={{ name: 'Jennifer O’Sullivan' }}
+        icon="chevron-right"
+      />
+      <Divider inset as="li" />
+      <ListItem
         primary="Matt Powell"
         secondary="Registration co-ordinator"
-        avatar={{
-          name: 'Matt Powell',
-          image: { small, medium },
-        }}
+        avatar={{ name: 'Matt Powell' }}
         icon="chevron-right"
       />
     </ul>
