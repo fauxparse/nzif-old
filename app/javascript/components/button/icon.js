c09ad012836785c1d592c1c15bb226205cb264fd
Matt Powell <fauxparse@gmail.com>
2019-06-29 16:24:05 +1200

    remove old button

--- app/javascript/components/button/icon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classNames from 'classnames'
-import Icon from '../../atoms/icon'
-
-const ButtonIcon = ({ className, children, ...props }) => (
-  <Icon className={classNames('button__icon', className)} {...props} />
-)
-
-export default ButtonIcon
