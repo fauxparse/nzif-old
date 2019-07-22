11bb594c5055bce1f0ac290d6d40a2fd4e3ab01d
Matt Powell <fauxparse@gmail.com>
2019-07-22 17:31:46 +0900

    admin sidebar links

--- app/javascript/components/admin/sidebar.js
+++ app/javascript/components/admin/sidebar.js
@@ -1,4 +1,44 @@
-import Sidebar from '../festivals/sidebar'
+import React from 'react'
+import PropTypes from 'lib/proptypes'
+import ReactRouterPropTypes from 'react-router-prop-types'
+import { withRouter } from 'react-router-dom'
+import Sidebar from 'organisms/sidebar'
+import List from 'molecules/list'
+import Divider from 'atoms/divider'
 
-export default Sidebar
+const AdminSidebar = ({ match, history, location, staticContext, festival, ...props }) => {
+  const { year } = festival || {}
 
+  return (
+    <Sidebar {...props}>
+      <List>
+        <List.Link to={`/${year}`} icon="arrow-left" primary="Festival home" />
+      </List>
+      <Divider inset />
+      <List>
+        <List.Link to={match.url} icon="admin" primary="Dashboard" />
+        <List.Link to={`${match.url}/pitches`} icon="pitch" primary="Pitches" />
+      </List>
+    </Sidebar>
+  )
+}
+
+AdminSidebar.propTypes = {
+  open: PropTypes.bool,
+  onClickOutside: PropTypes.func,
+  festival: PropTypes.shape({
+    pitchesOpen: PropTypes.bool,
+  }),
+  history: ReactRouterPropTypes.history.isRequired,
+  location: ReactRouterPropTypes.location.isRequired,
+  match: ReactRouterPropTypes.match.isRequired,
+}
+
+AdminSidebar.defaultProps = {
+  onClickOutside: undefined,
+  festival: {
+    pitchesOpen: false,
+  },
+}
+
+export default withRouter(AdminSidebar)
