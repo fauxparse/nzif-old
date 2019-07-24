71ddee02e267f157697a1cb6edf455058bd7cf17
Matt Powell <fauxparse@gmail.com>
2019-07-25 09:30:39 +1200

    remove old ripple code

--- app/javascript/components/shared/menu/separator.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classNames from 'classnames'
-
-const Separator = ({ className, ...props }) => (
-  <hr className={classNames('menu__separator', className)} {...props} />
-)
-
-export default Separator
