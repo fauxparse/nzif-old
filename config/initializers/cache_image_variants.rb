ce64f2e1cbd32b605a379d2e7dc2f47a33e32f7a
Matt Powell <fauxparse@gmail.com>
2019-09-06 22:03:13 +1200

    fix autoload warning in development

--- config/initializers/cache_image_variants.rb
+++ config/initializers/cache_image_variants.rb
@@ -1,13 +1,15 @@
-module ActiveStorage
-  class RepresentationsController < BaseController
-    include ActiveStorage::SetBlob
+Rails.configuration.to_prepare do
+  module ActiveStorage
+    class RepresentationsController < BaseController
+      include ActiveStorage::SetBlob
 
-    def show
-      expires_in 1.year, public: true
-      variant = @blob.representation(params[:variation_key]).processed
-      send_data @blob.service.download(variant.key),
-        type: @blob.content_type || DEFAULT_SEND_FILE_TYPE,
-        disposition: 'inline'
+      def show
+        expires_in 1.year, public: true
+        variant = @blob.representation(params[:variation_key]).processed
+        send_data @blob.service.download(variant.key),
+          type: @blob.content_type || DEFAULT_SEND_FILE_TYPE,
+          disposition: 'inline'
+      end
     end
   end
 end
