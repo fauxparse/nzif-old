ea55df3ef3f1ec63626f279f192c58991d7cf74d
Matt Powell <fauxparse@gmail.com>
2019-09-10 15:33:50 +1200

    matcher spec

--- spec/interactors/match_workshops_spec.rb
+++ spec/interactors/match_workshops_spec.rb
@@ -1,7 +1,42 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe MatchWorkshops, type: :interactor do
+  subject(:result) do
+    MatchWorkshops.call(festival: festival, seed: seed)
+  end
+
+  let(:festival) { create(:festival) }
+
+  let(:workshops) { create_list(:workshop, 3, festival: festival) }
+
+  let!(:sessions) do
+    workshops.map { |workshop| create(:session, activity: workshop, capacity: 5) }
+  end
+
+  let!(:registrations) do
+    srand(seed)
+    create_list(:registration, 16, festival: festival).each do |registration|
+      sessions.shuffle[0, rand(sessions.size) + 1].each do |session|
+        registration.preferences.create!(session: session)
+      end
+    end
+  end
+
+  let(:seed) { 12345 }
+
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it { is_expected.to be_success }
+  end
+
+  describe '#matches' do
+    subject(:matches) { result.matches }
+
+    it { is_expected.to include(sessions.first.starts_at) }
+
+    describe '[\'unallocated\']' do
+      subject(:unallocated) { matches[sessions.first.starts_at]['unallocated'] }
+
+      it { is_expected.to have(1).item }
+    end
   end
 end
