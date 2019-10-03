24fbdd8192d1e3d2597158e9a779a947cc34e9b7
Matt Powell <fauxparse@gmail.com>
2019-10-04 10:37:40 +1300

    incident report form

--- app/javascript/pages/code_of_conduct/index.js
+++ app/javascript/pages/code_of_conduct/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import ReactRouterPropTypes from 'react-router-prop-types'
 import gql from 'graphql-tag'
 import get from 'lodash/get'
-import { useQuery } from 'react-apollo'
+import { useQuery, useMutation } from 'react-apollo'
 import { useStaticContent } from 'contexts/static_content'
 import NotFound from 'templates/not_found'
 import Template from 'templates/code_of_conduct'
@@ -15,12 +15,26 @@ const GET_ACCEPTED_AT = gql`
   }
 `
 
+const SEND_REPORT = gql`
+  mutation sendReport($year: ID!, $body: String!, $anonymous: Boolean) {
+    createIncident(year: $year, body: $body, anonymous: $anonymous) {
+      id
+    }
+  }
+`
+
 const CodeOfConduct = ({ match }) => {
   const { year } = match.params
 
   const { data } = useQuery(GET_ACCEPTED_AT, { variables: { year } })
 
   const { loading, raw, title, error } = useStaticContent('code-of-conduct')
+
+  const [sendReportMutation] = useMutation(SEND_REPORT)
+
+  const sendReport = useCallback((variables) => (
+    sendReportMutation({ variables: { year, ...variables } })
+  ), [sendReportMutation, year])
 
   if (!loading && error) {
     return <NotFound />
@@ -33,6 +47,7 @@ const CodeOfConduct = ({ match }) => {
       title={title}
       raw={raw}
       acceptedAt={get(data, 'registration.codeOfConductAcceptedAt')}
+      onSendReport={sendReport}
     />
   )
 }
--- app/javascript/templates/code_of_conduct/index.js
+++ app/javascript/templates/code_of_conduct/index.js
@@ -1,20 +1,25 @@
-import React, { useRef } from 'react'
+import React, { Fragment, useRef } from 'react'
 import PropTypes from 'lib/proptypes'
 import { Link } from 'react-router-dom'
-import Header from 'organisms/header'
-import Markdown from 'molecules/markdown'
-import Skeleton from 'effects/skeleton'
 import { lorem } from 'faker'
 import { usePermission } from 'lib/permissions'
+import { useToggle } from 'lib/hooks'
 import Date from 'atoms/date'
+import Button from 'atoms/button'
 import Breadcrumbs from 'molecules/breadcrumbs'
+import Markdown from 'molecules/markdown'
+import Header from 'organisms/header'
+import Skeleton from 'effects/skeleton'
+import Report from './report'
 
 import './index.scss'
 
-const CodeOfConduct = ({ loading, raw, title, year, acceptedAt }) => {
+const CodeOfConduct = ({ loading, raw, title, year, acceptedAt, onSendReport }) => {
   const loadingText = useRef(lorem.paragraphs(5))
 
   const canEdit = usePermission('editContent')
+
+  const [reporting, , showReport, hideReport] = useToggle()
 
   return (
     <section className="static-content__page code-of-conduct">
@@ -28,6 +33,9 @@ const CodeOfConduct = ({ loading, raw, title, year, acceptedAt }) => {
         {canEdit && (
           <Header.Button as={Link} to={`/admin/${year}/content/code-of-conduct`} icon="edit" />
         )}
+        <Fragment>
+          <Button icon="alert" text="Report an incident" onClick={showReport} />
+        </Fragment>
       </Header>
       {acceptedAt && (
         <div className="code-of-conduct__accepted">
@@ -42,6 +50,11 @@ const CodeOfConduct = ({ loading, raw, title, year, acceptedAt }) => {
           text={loading ? loadingText.current : raw}
         />
       </div>
+      <Report
+        open={reporting}
+        onCancel={hideReport}
+        onSend={onSendReport}
+      />
     </section>
   )
 }
@@ -52,6 +65,7 @@ CodeOfConduct.propTypes = {
   raw: PropTypes.string,
   year: PropTypes.id.isRequired,
   acceptedAt: PropTypes.time,
+  onSendReport: PropTypes.func.isRequired,
 }
 
 CodeOfConduct.defaultProps = {
