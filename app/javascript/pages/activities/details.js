8c5c57ff33010284c8d80454739d710d10120123
Matt Powell <fauxparse@gmail.com>
2019-10-07 16:45:35 +1300

    handle not found on activity details

--- app/javascript/pages/activities/details.js
+++ app/javascript/pages/activities/details.js
@@ -1,8 +1,10 @@
 import React, { useMemo } from 'react'
 import ReactRouterPropTypes from 'react-router-prop-types'
+import { Redirect } from 'react-router-dom'
 import { useQuery } from 'react-apollo-hooks'
 import snakeCase from 'lodash/snakeCase'
 import Template from 'templates/activities/details'
+import NotFound from 'templates/not_found'
 import ACTIVITY_QUERY from 'queries/activity'
 
 const Details = ({ match }) => {
@@ -10,9 +12,18 @@ const Details = ({ match }) => {
 
   const type = snakeCase(match.params.type).replace(/s$/, '')
 
-  const { loading, data } = useQuery(ACTIVITY_QUERY, { variables: { year, type, slug } })
+  const { loading, data, error } = useQuery(ACTIVITY_QUERY, { variables: { year, type, slug } })
 
   const activity = useMemo(() => (!loading && data && data.activity) || undefined, [loading, data])
+
+  if (error) {
+    const graphQLError = error.graphQLErrors[0]
+    if (graphQLError.status === 'NOT_FOUND') {
+      return <NotFound />
+    } else {
+      return <Redirect to="/" />
+    }
+  }
 
   return (
     <Template
