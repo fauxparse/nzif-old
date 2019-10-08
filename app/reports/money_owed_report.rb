d0ed9205608413f26ad24a44adc15e4a7524e974
Matt Powell <fauxparse@gmail.com>
2019-10-09 08:59:38 +1300

    money owed report

---
+++ app/reports/money_owed_report.rb
@@ -0,0 +1,15 @@
+class MoneyOwedReport < Report::Base
+  field(:name) { |row| row.user.name }
+  field(:email) { |row| row.user.email }
+  field(:total) { |row| Registration::PRICES[row.placements_count] / 100 }
+  field(:paid) { |row| row.payments.select(&:approved?).sum(&:amount).to_i }
+
+  def scope
+    festival
+      .registrations
+      .complete
+      .includes(:user, :payments)
+      .references(:users)
+      .order('users.name ASC')
+  end
+end
