c0aa234ee6177bcd9fd94b85d8f63e5fc184aca2
Matt Powell <matt.powell@optimalworkshop.com>
2021-06-17 15:16:12 +1200

    upgrade sendgrid to use API key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
     :domain => 'improvfest.nz',
     :address => 'smtp.sendgrid.net',
     :port => 587,
